af6012eadd0a2c27c74bfb290099831cd585c2b9
wayne-daniels <wa.daniels@hotmail.com>
2020-05-06 21:54:22 +0000

    Completed User Can View the Average Grade Feature

--- components/app.js
+++ components/app.js
@@ -11,6 +11,12 @@ class App {
   }
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades);
+    var gradeTotal = 0;
+    for (var i = 0; i < grades.length; i++) {
+      gradeTotal += grades[i].grade;
+    }
+    var avGrade = gradeTotal / grades.length;
+    this.pageHeader.updateAverage(avGrade);
   }
   getGrades() {
     $.ajax(
--- components/page-header.js
+++ components/page-header.js
@@ -3,6 +3,7 @@ class PageHeader {
     this.headerElement = headerElement;
   }
   updateAverage(newAverage) {
-    console.log(newAverage);
+    var grAv = document.querySelector("span");
+    grAv.textContent = newAverage;
   }
 }
